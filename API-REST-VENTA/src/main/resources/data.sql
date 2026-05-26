e376ad17b140958c2ba812fef3cfc0c3104f5a31
lucciano martinez <zorrapeluahihu@gmail.com>
2026-05-25 23:10:05 -0400

    config: Agrega data.sql para pruebas

--- API-REST-VENTA/src/main/resources/data.sql
+++ API-REST-VENTA/src/main/resources/data.sql
@@ -1,11 +1,12 @@
 DELETE FROM venta;
 
-INSERT INTO venta (
-    direccion_compra,
-    valor_compra,
-    fecha_compra,
-    despacho_generado
+INSERT IGNORE INTO venta (
+  id_venta,
+  direccion_compra,
+  valor_compra,
+  fecha_compra,
+  despacho_generado
 ) VALUES
-      ('Av. Siempre Viva 123', 25990, '2026-05-25', false),
-      ('Los Carrera 456', 49990, '2026-05-25', true),
-      ('Brasil 789', 12990, '2026-05-25', false);
+(1, 'Av. Siempre Viva 123', 25990, '2026-05-25', false),
+(2, 'Los Carrera 456', 49990, '2026-05-25', true),
+(3, 'Brasil 789', 12990, '2026-05-25', false);12990, '2026-05-25', false);
